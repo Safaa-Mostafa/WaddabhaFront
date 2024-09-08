594eb21cbfec3efbaf66e4fb88fab57a216a6fd8
Nada Hassan <nadahassanhossini@gmail.com>
2024-09-08 15:28:58 +0300

    adding new service

--- src/app/pages/website/service/services/service.service.ts
+++ src/app/pages/website/service/services/service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Service } from '../models/service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ServiceService {
-
-  constructor() { }
+  private urlPath = 'https://localhost:7116/api/Services';
+  constructor(private http: HttpClient) {}
+  //  addService () : Observable<any>{
+  //   return this.http.post<Service>(this.urlPath)
+  //  }
 }
