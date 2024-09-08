c03d02360db2f0552c88168f1a3f492c99fd8093
mohamedAyman00 <ma1090560@gmail.com>
2024-09-08 17:54:38 +0300

    Add All-Services Component and make the style

--- src/app/pages/website/service/all-services/all-services.component.ts
+++ src/app/pages/website/service/all-services/all-services.component.ts
@@ -1,19 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../services/service.service';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-all-services',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './all-services.component.html',
-  styleUrl: './all-services.component.css'
+  styleUrls: ['./all-services.component.css']
 })
 export class AllServicesComponent implements OnInit {
-constructor(private service : ServiceService){}
-ngOnInit(): void {
-  this.service.getAllServices().subscribe(function (e){
-    console.log(e);
-    
-  })
-}
+  services: any[] = [];
+
+  constructor(private service: ServiceService) { }
+
+  ngOnInit(): void {
+    this.service.getAllServices().subscribe({
+      next: (res) => {
+        this.services = res.data; // Assuming `res.data` contains the array of services
+        console.log(this.services);
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  getStarArray(rating: number): string[] {
+    const fullStars = Math.floor(rating);
+    const halfStars = rating % 1 >= 0.5 ? 1 : 0;
+    const emptyStars = 5 - fullStars - halfStars;
+
+    const stars = [];
+
+    // Add full stars
+    for (let i = 0; i < fullStars; i++) {
+      stars.push('full');
+    }
+
+    // Add half star if needed
+    if (halfStars) {
+      stars.push('half');
+    }
+
+    // Add empty stars
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push('empty');
+    }
+
+    return stars;
+  }
 }
--- src/app/pages/website/service/service-details/service-details.component.ts
+++ src/app/pages/website/service/service-details/service-details.component.ts
@@ -1,24 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../services/service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-service-details',
   standalone: true,
-  imports: [],
   templateUrl: './service-details.component.html',
-  styleUrl: './service-details.component.css'
+  styleUrls: ['./service-details.component.css']
 })
-export class ServiceDetailsComponent {
-  images = [
-    'assets/images/img1.jpg',
-    'assets/images/img2.jpg',
-    './assets/images/img3.jpg'
-  ];
-  currentImage = this.images[0];
-  currentIndex = 0;
+export class ServiceDetailsComponent implements OnInit {
+  service: any = {};  // This will hold the service details
+  images: string[] = ["/assets/daman.png"];
+  currentImage: string = '';
+  currentIndex: number = 0;
   intervalId: any;
 
+  constructor(private serviceService: ServiceService, private activeRouter: ActivatedRoute) { }
+
   ngOnInit(): void {
-    this.startAutoSwitch();
+    this.activeRouter.paramMap.subscribe({
+      next: (params) => {
+        const id: any = params.get('id');
+        this.serviceService.getId(id).subscribe({
+          next: (response) => {
+            this.service = response.data;
+            this.images = this.service.images;  // Assuming `images` is an array in your API response
+            this.currentImage = this.images[0];
+            this.startAutoSwitch();
+          },
+          error: (err) => {
+            console.log(err);
+          }
+        });
+      }
+    });
   }
 
   ngOnDestroy(): void {
@@ -38,6 +53,6 @@ export class ServiceDetailsComponent {
   startAutoSwitch(): void {
     this.intervalId = setInterval(() => {
       this.nextImage();
-    }, 4000); // Switch image every 3 seconds
+    }, 4000); // Switch image every 4 seconds
   }
 }
