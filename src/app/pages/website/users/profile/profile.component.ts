3d80d5b8bdb51335c45a52e073775cf3bddf1881
HebaShahin <hebashahin1999@gmail.com>
2024-09-12 19:13:12 +0300

    adduserprofile

--- src/app/pages/website/users/profile/profile.component.ts
+++ src/app/pages/website/users/profile/profile.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Init } from 'v8';
+import { User } from '../../auth/Models/user';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
+  user: User | null = null;
+
+
+  constructor( private router: Router, private userServcie: UserService){}
+
+  ngOnInit() {
+
+    this.user = this.userServcie.getStoredUserData();
+    console.log(this.user);
+  }
 }
